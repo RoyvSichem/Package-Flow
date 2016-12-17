1881bac8e784c05c25adf88aa327f2a206362571
Roy van Sichem <royvs@live.nl>
2016-12-17 13:39:13 +0000

    articles toegevoegd

---
+++ db/migrate/20161212184858_add_description_to_articles.rb
@@ -0,0 +1,9 @@
+class AddDescriptionToArticles < ActiveRecord::Migration
+  def change
+    
+    # table name, atribute, type
+    add_column :articles, :description, :text
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime  
+  end
+end
