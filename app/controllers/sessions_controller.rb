b12ceb4b76096b6303390219cd055bf98a7da5ed
Roy van Sichem <royvs@live.nl>
2017-01-10 18:37:05 +0000

    det the autentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   
   def create
     
-    user = User.find_by(username: params[:session][:username].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You successfully logged in"
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
       render 'new'
     end
       
-    end
+  end
   
   def destroy
     session[:user_id] = nil
